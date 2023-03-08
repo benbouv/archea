ec8c8cb5aa6075b11eac621f5f484a109021522f
on99833 <on99833@outlook.fr>
2023-03-08 11:21:05 +0100

    save security

--- migrations/Version20230308092547.php
+++ migrations/Version20230308092547.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230307151448 extends AbstractMigration
+final class Version20230308092547 extends AbstractMigration
 {
     public function getDescription(): string
     {
