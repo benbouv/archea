f04fb756f7b5298b72e5c144d0932ad69fb7569a
on99833 <on99833@outlook.fr>
2023-03-09 13:40:32 +0100

    save7

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\GeolocalisationRepository;
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'app_map')]
-    public function index(): Response
+    public function index(PhotoRepository $photoRepository, GeolocalisationRepository $geolocalisationRepository): Response
     {
+
+        $photos = $photoRepository->findAll();
+        $gisments = $geolocalisationRepository->findAll();
+
         return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
+            'photos' => $photos,
+            'gisments' => $gisments
         ]);
     }
 }
